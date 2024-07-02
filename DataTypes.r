984f6a4d2b10f909050397755e1af6ccee3f3cf7
Brian342 <migelbrian3@gmail.com>
2024-07-02 11:53:41 +0300

    Data types

--- DataTypes.r
+++ DataTypes.r
@@ -4,9 +4,19 @@
 # - boolean values/ Logical values eg (TRUE, FALSE)
 # - Complex Types eg 2+5!
 
+# Numeric data type
 my_obj <- 123
 class(my_obj)
 print(class)
 
+# character data type
 my_obj <- "Briaaaaaaan"
+print(class(my_obj))
+
+# logical data types
+my_obj <- TRUE
+print(class(my_obj))
+
+# complex data types
+my_obj <- 2 + 5i
 print(class(my_obj))
