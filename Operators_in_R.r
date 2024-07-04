ecec5f70f2ddb32e6c96a31727fea7963c8b51a5
Brian342 <migelbrian3@gmail.com>
2024-07-05 02:58:39 +0300

    Operators

---
+++ Operators_in_R.r
@@ -0,0 +1,16 @@
+# Assignment Operators - used to assign a value to an object <-, -> =
+# Arithmetic Operators
+# Relational Operators
+# Logical Operators
+
+# Arithmetic operator
+x = 10 # nolint: assignment_linter.
+y <- 20 # variable y is assigned value 20
+30 -> z # nolint.  assined 30 to variable z
+
+print(x)
+
+# Arithmetic operators are used to
+# perform basic mathematical operations
+# like addition (+), subtraction(-), multiplication(*), division(/) etc.
+
