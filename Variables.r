53d246244de319d4dd47b045465e1c2f956294ad
Brian342 <migelbrian3@gmail.com>
2024-07-10 07:42:25 +0300

    Operators

--- Variables.r
+++ Variables.r
@@ -1,1 +1,11 @@
-print(x)
+# Variables are used to store data with named locations 
+# that your programs can manipulate. a variable name can be a 
+# combination of letters,digits,periods and underscore
+model1 <-'hello'
+typeof(model1)
+class(model1)
+.pairs <- 10
+.pairs
+hellostring <- 'real hello'
+hellostring
+# no spacing between the variable name
