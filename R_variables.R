95f86ba6b8f77219ff4d8c9c1e335bf0922bdeaf
Brian kimanzi <migelbrian3@gmail.com>
2025-05-28 15:54:24 +0300

    New code update

---
+++ R_variables.R
@@ -0,0 +1,25 @@
+# A variable provides us with named storage that our programs can manipulate.
+# A variable in R can store an atomic vector, group of atomic vectors or a 
+# combination of many R-objects.A valid variable name consists of letters, 
+# numbers and the dot or underline characters. the variable name start with a 
+# letter or the dot not followed by a number
+# var_name2. valid. Has letters, numbers, dot and underscore
+# var_name% Invalid. Has the character '%'. Only dot(.) and underscore allowed.
+# 2var_name invalid. Starts with a number
+#.var_name ,var.name valid. Can start with a dot(.) but the dot(.)should not be followed by a number.
+# .2var_name invalid. The starting dot is followed by a number making it invalid
+# _var_name invalid. Starts with _ which is not valid
+
+   
+ # Variable Assignment
+# The variable can be assigned values using leftward, rightward, and equal to operator.
+# The values of the variables can be printed using Print() or cat() function. The Cat() function
+# combines multiple items into a continuous print output
+
+# Assignment using equal operator
+var.1 = c(0,1,2,3)
+
+# Assignment using leftward operator
+var.2 <- c("learn", "R")
+
+#
---
+++ log_and.R
@@ -0,0 +1,10 @@
+# logarithms and exponentials
+# when supplied a given number x and a value referred to as base, the logarithm calculates
+# the power to which you must raise the base to get to x
+# eg logarithm of x = 243 to base 3
+# log(x=243, base = 3)
+# Things to consider 
+# both x and the base must be positive, 
+# The log of any number x when the base is equal to x is 1
+# the log of x = 1 is always 0, regardless of the base
+
