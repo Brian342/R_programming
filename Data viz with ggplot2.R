9dc6335289ed14053740ad1e8fe1538dbc14e217
Brian kimanzi <migelbrian3@gmail.com>
2025-05-30 04:58:01 +0300

    new code

--- Data viz with ggplot2.R
+++ Data viz with ggplot2.R
@@ -1,2 +1,19 @@
-ggplot(data=mpg)+
-  geom_point(mapping = aes(x=displ, y=hwy, colour=class))
+# ggplot(data=mpg)+
+#   geom_point(mapping = aes(x=displ, y=hwy, colour=class))
+
+# ggplot(data=mpg)+
+#   geom_point(mapping = aes(x = displ, y= hwy, size=class))
+
+# top
+# ggplot(data=mpg)+
+#  geom_point(mapping = aes(x = displ, y=hwy, alpha = class))
+
+# bottom
+#ggplot(data=mpg)+
+#   geom_point(mapping = aes(x = displ, y=hwy, shape = class))
+
+#ggplot(data = mpg)+
+#   geom_point(mapping = aes(x = displ, y = hwy), colour = "blue")
+
+
+# ?mpg
